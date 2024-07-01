3f78e37fc6a611136ef63b6c93e38bd671ccfffb
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-07-01 10:53:34 -0400

    testing

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        int j, k;
+        j = 10;
+        j = 5;;
+        k = j;
+        System.out.println("j is : " + j);
+        System.out.println("k is : " + k);
+
+
+        k = j = 10;
+        System.out.println("j is : " + j);
+        System.out.println("k is : " + k);
+
+
+
+
+
+
+
+        }
+    }
